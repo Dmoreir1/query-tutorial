2d6a4cd32589422a7239caeb9c7191ae7a703211
dmoreir1 <dmoreir1@binghamton.edu>
2023-05-31 18:48:29 -0400

    finishing tutorial

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from "react-query-devtools";
 
+const queryClient = new QueryClient({});
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <QueryClientProvider client = {queryClient}>
+
+      <ReactQueryDevtools initialIsOpen={true}/>
+    </QueryClientProvider>
+  )
 }
 
+
 export default App;
+
+	
+// const fetchedData = useQuery(key, promiseBasedFn);
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
